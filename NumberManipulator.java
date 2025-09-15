ff5458f5c9633e5cee1a1dc73aa12a30645aa895
Clarence Bunting <cbbunting1@my.waketech.edu>
2025-09-15 09:24:39 -0400

    Three adds later i'm done

--- NumberManipulator.java
+++ NumberManipulator.java
@@ -1,20 +1,41 @@
 import java.util.Scanner;
 
 public class NumberManipulator {
+    public static Scanner input = new Scanner(System.in);
+
+    public static double get_input(String mes){ //Used for getting user input
+        System.out.print(mes);
+        return input.nextDouble();
+    }
+
+    public static int round_up(double num){ //Rounds up
+        return (int) Math.ceil(num);
+    }
+
+    public static int round_down(double num){ //Rounds Down
+        return (int) Math.floor(num);
+    }
+
+    public static double round_decimal(double num){ //Rounds to nearest number returned as a decimal with ties going to the even number.
+        return Math.rint(num);
+    }
+
+    public static int round_int(double num){ //Rounds normally
+        return (int)Math.round(num);
+    }
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        double numero = get_input("Enter a decimal number: "); //Get number from user.
 
-        // Prompt the user to enter a decimal number
-        // Store the input in a variable
+        //For all the calling
+        int larint = round_up(numero);
+        int smlnum = round_down(numero);
+        double evenum = round_decimal(numero);
+        int rounum = round_int(numero);
 
-        // Print the smallest integer greater than or equal to the number
-        // Print the largest integer less than or equal to the number
-        // Print the integer that is closest to the number (ties go to even)
-        // Print the integer closest to the number (standard rounding)
-
-        // BONUS:
-        // Cast the number to an int and print the character it represents
-        // Add 1 to that number and print the next character
-        // Format a summary table using formatted output
+        //Prints out values
+        System.out.println("When rounding down: "+smlnum);
+        System.out.println("When rounding up: "+larint);
+        System.out.println("Normal rounding with even ties: "+evenum);
+        System.out.println("Normal rounding: "+ rounum);
     }
 }
